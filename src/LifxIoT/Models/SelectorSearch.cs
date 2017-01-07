5f8953e1ee59ce75e6aef795de215f0f48f6f50e
Matt Grant <matthew.grant@planning-inc.co.uk>
2017-01-07 23:47:58 +0000

    Added search selector.

---
+++ src/LifxIoT/Models/SelectorSearch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LifxIoT.Models
+{
+    public class SelectorSearch : Interfaces.ISelector
+    {
+        public string SearchType { get; set; }
+
+        public string SearchValue { get; set; }
+
+        public string GetSelectorText()
+        {
+            return SearchType + ":" + SearchValue;
+        }
+    }
+}
